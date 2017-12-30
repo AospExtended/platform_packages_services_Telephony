DarkobasOrg/android_packages_services_Telephony

Telephony: check before removing preference
Change-Id: Iae4da0adacca92fbcd4744bd162f7540dfbf9c27

src/com/android/phone/CallFeaturesSetting.java
@@ -418,7 +418,7 @@ public class CallFeaturesSetting extends PreferenceActivity
             /* tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE); */
         }
 
-        if (!PackageUtils.isAppInstalled(this, "com.qualcomm.qti.ims")) {
+        if (!PackageUtils.isAppInstalled(this, "com.qualcomm.qti.ims") && findPreference("ims_settings_key") != null) {
             prefSet.removePreference(findPreference("ims_settings_key"));
         }
 

AospExtended/platform_packages_services_Telephony

src/com/android/phone/CallFeaturesSetting.java
@@ -413,7 +413,7 @@ public class CallFeaturesSetting extends PreferenceActivity
             /* tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE); */
         }
 
-        if (!AEXUtils.isPackageInstalled(this, "com.qualcomm.qti.ims")) {
+        if (!AEXUtils.isPackageInstalled(this, "com.qualcomm.qti.ims") && findPreference("ims_settings_key") != null) {
             prefSet.removePreference(findPreference("ims_settings_key"));
         }
 